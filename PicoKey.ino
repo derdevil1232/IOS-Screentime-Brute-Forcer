95eb316dc503d8c7c22b5e7c7456a74e5b36af83
Joe <202152720+derdevil1232@users.noreply.github.com>
2026-08-01 21:51:11 +0100

    Update PicoKey.ino

--- PicoKey.ino
+++ PicoKey.ino
@@ -4,16 +4,10 @@ const int LED = LED_BUILTIN;
 
 void setup() {
   pinMode(LED, OUTPUT);
-
-  // Starts serial
   Serial.begin(115200);
-  // Small delay to let PC serial monitor connect if plugged in
   delay(1000);
-
-  // start BLE keyboard
   KeyboardBLE.begin("PicoKey");
 
-  // 30 second safety countdown
   for (int i = 0; i < 30; i++) {
     digitalWrite(LED, HIGH);
     delay(500);
@@ -22,21 +16,14 @@ void setup() {
   }
   digitalWrite(LED, HIGH);
 
-  // Run combinations
   for (int i = 0; i < 10000; i++) {
     char combo[5];
     snprintf(combo, sizeof(combo), "%04d", i);
-
-    // Send over BLE to the phone
     KeyboardBLE.print(combo);
     KeyboardBLE.write(KEY_RETURN);
-
-    // Mirror to PC serial (if itsconnected)
     Serial.println(combo);
-
     delay(500);
   }
-
   digitalWrite(LED, LOW);
 }
 
